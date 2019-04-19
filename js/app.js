bc697e504d2108c5f5463590dc81e403608061ad
Fatemeh Charlangi <fatemhcharlangi@gmail.com>
2019-04-19 12:36:49 +0430

    add some js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 
-// Salam Eshgham
+const menuButton    = document.querySelector('.menuToggler')
+const menuButtonIcon    = menuButton.querySelector('i')
+const sidebar       = document.querySelector('.sidebar')
+
+menuButton.addEventListener('click', () => {
+    sidebar.classList.toggle('opened')
+    menuButton.classList.toggle('toggle')
+    menuButtonIcon.classList.replace('fa-bars', 'fa-times')
+})
+
+
+const subLinkOpener = document.querySelector('.subLinkOpener')
+const subLink = subLinkOpener.nextElementSibling
+const arrow = subLinkOpener.querySelector('.arrow')
+
+console.log(arrow)
+
+subLinkOpener.addEventListener('click', () => {
+    subLink.classList.toggle('opened')
+    arrow.classList.toggle('opened')
+})
